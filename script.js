ec73fc0187f8c7435cd300d5412915ff2919b1a1
Puspa Wahyuningtias <puspa.wahyuningtias@gmail.com>
2020-08-16 22:11:49 +0700

    memperbaiki header main footer untuk upload dicoding 16/08

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ const nav = document.querySelector('nav ul');
 menuToggle.addEventListener('click', function() {
 	nav.classList.toggle('slide');
 });
+
+const gambar = document.querySelectorAll('.gambar');
+const article = document.querySelectorAll('article');
+const artikel = document.querySelectorAll('article .text');
+var x = window.matchMedia("(max-width: 768px)");
+myFunction(x);
+x.addListener(myFunction);
+
+function myFunction(x){
+	if (x.matches) {
+		for (var i = 0; i < gambar.length; i++) {
+			article[i].insertBefore(gambar[i], artikel[i])
+		}
+	}
+}
+
+
+
+
